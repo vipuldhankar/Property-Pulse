201754ecf7030ec5661ff1c3f10977ddc892bbe5
vipuldhankar <vipulkumar1823@gmail.com>
2025-05-23 14:27:44 +0530

    metadate adds

---
+++ property-pulse/app/layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "@/assets/styles/globals.css";
+export const metadata = {
+       title: 'PropertyPulse | Find The Perfect Rental',
+       description: 'Find your dream rental property',
+       keywords: 'rental, find rentals, find properties',
+     };
+const Mainlayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default Mainlayout;
