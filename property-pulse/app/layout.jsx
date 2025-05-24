cd1bf791fff72fbe3eb0f8bb08a6d54f08be8545
vipuldhankar <vipulkumar1823@gmail.com>
2025-05-24 19:06:46 +0530

    makes navbar footer properties and others

--- property-pulse/app/layout.jsx
+++ property-pulse/app/layout.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import Navbar from '@/components/Navbar';
 import "@/assets/styles/globals.css";
+import Footer from '@/components/Footer';
 export const metadata = {
-       title: 'PropertyPulse | Find The Perfect Rental',
-       description: 'Find your dream rental property',
-       keywords: 'rental, find rentals, find properties',
-     };
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
 const Mainlayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div>{children}</div>
+        <Navbar />
+        <main>{children}</main>
+        <Footer/>
       </body>
     </html>
   );
--- property-pulse/app/page.jsx
+++ property-pulse/app/page.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div>
+    <Hero/>
+    <InfoBoxes/>
+    <HomeProperties/>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
