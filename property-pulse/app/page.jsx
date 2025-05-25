86867da8ae1f15890e771479d57e1b660f05ac67
vipuldhankar <vipulkumar1823@gmail.com>
2025-05-25 14:14:51 +0530

    database connection makes env file and config file

--- property-pulse/app/page.jsx
+++ property-pulse/app/page.jsx
@@ -3,15 +3,14 @@ import Link from "next/link";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
-
-const Homepage = () => {
+const Homepage = async () => {
   return (
     <div>
-    <Hero/>
-    <InfoBoxes/>
-    <HomeProperties/>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
     </div>
   );
-};
+}; 
 
 export default Homepage;
---
+++ property-pulse/config/database.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+let connected = false;
+const connectDB = async () => {
+       mongoose.set("strictQuery", true);
+       // if database is already connected, then don't connect again
+       if (connected) {
+              console.log("Already connected to MongoDB");
+              return
+       };
+       try {
+              await mongoose.connect(process.env.MONGODB_URI);
+              connected = true;
+              console.log("Connected to MongoDB");
+       } catch (error) {
+              console.error(`Error connecting to MongoDB: ${error.message}`);
+              process.exit(1);
+       }
+
+
+}
+
+export default connectDB;
