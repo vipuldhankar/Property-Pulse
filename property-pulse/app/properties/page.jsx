00ee94660b0de23df1dcde1d0ac1ecadb2d4c261
vipuldhankar <vipulkumar1823@gmail.com>
2025-05-31 22:34:57 +0530

    Search functionality done

--- property-pulse/app/properties/page.jsx
+++ property-pulse/app/properties/page.jsx
@@ -1,28 +1,37 @@
 import PropertyCard from "@/components/PropertyCard";
 import { fetchproperties } from "@/utils/requests";
-import { getSessionUser } from '@/utils/getSessionUser';
-import cloudinary from '@/config/cloudinary';
+import { getSessionUser } from "@/utils/getSessionUser";
+import cloudinary from "@/config/cloudinary";
+import PropertySearchForm from "@/components/PropertySearchForm";
 
 const PropertiesPage = async () => {
   const response = await fetchproperties();
-  const properties = response.properties || [];  // Access the properties array
+  const properties = response.properties || []; // Access the properties array
 
   properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No Properties Found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p>No Properties Found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
