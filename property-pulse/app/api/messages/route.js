95a5f81724177b1a2188a79dc5903fd9b888a1bd
vipuldhankar <vipulkumar1823@gmail.com>
2025-06-02 19:05:37 +0530

    message delete ,mark as read or new , order the messages as there time is done and working

--- property-pulse/app/api/messages/route.js
+++ property-pulse/app/api/messages/route.js
@@ -19,7 +19,10 @@ export const GET = async () => {
 
     const { userId } = sessionUser;
 
-    const readMessages = await Message.find({ recipient: userId, read: true })
+    const readMessages = await Message.find({
+      recipient: userId,
+      read: true
+    })
       .sort({ createdAt: -1 }) // Sort read messages in asc order
       .populate('sender', 'username')
       .populate('property', 'name');
--- property-pulse/components/Message.jsx
+++ property-pulse/components/Message.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 const Message = ({ message }) => {
+  const [isRead, setIsRead] = useState(message.read);
+      const [isDeleted, setIsDeleted] = useState(false);
+
+  const handleReadClick = async () => {
+    try {
+      const res = await fetch(`/api/messages/${message._id}`, {
+        method: "PUT",
+      });
+
+      if (res.status === 200) {
+        const { read } = await res.json();
+        setIsRead(read);
+       //  setUnreadCount((prevCount) => (read ? prevCount - 1 : prevCount + 1));   
+        if (read) {
+          toast.success("Marked as read");
+        } else {
+          toast.success("Marked as new");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+       const handleDeleteClick = async () => {
+         try {
+           const res = await fetch(`/api/messages/${message._id}`, {
+             method: 'DELETE',
+           });
+
+           if (res.status === 200) {
+             setIsDeleted(true);
+       //       setUnreadCount((prevCount) => prevCount - 1);
+             toast.success('Message Deleted');
+           }
+         } catch (error) {
+           console.log(error);
+           toast.error('Message was not deleted');
+         }
+       };
+
+       if (isDeleted) {
+         return null;
+       }
+
   return (
     <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
+      {!isRead && (
+        <div className="absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md">
+          New
+        </div>
+      )}
       <h2 className="text-xl mb-4">
         <span className="font-bold">Property Inquiry:</span>
         {message.property.name}
@@ -27,14 +80,22 @@ const Message = ({ message }) => {
           </a>
         </li>
         <li>
-          <strong>Received:</strong>{' '}
+          <strong>Received:</strong>{" "}
           {new Date(message.createdAt).toLocaleString()}
         </li>
       </ul>
-      <button className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md">
-        Mark As Read
+      <button
+        onClick={handleReadClick}
+        className={`mt-4 mr-3 ${
+          isRead ? "bg-gray-300" : "bg-blue-500 text-white"
+        } py-1 px-3 rounded-md`}
+      >
+        {isRead ? "Mark As New" : "Mark As Read"}
       </button>
-      <button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
+      <button
+        onClick={handleDeleteClick}
+        className='mt-4 bg-red-500 text-white py-1 px-3 rounded-md'
+      >
         Delete
       </button>
     </div>
