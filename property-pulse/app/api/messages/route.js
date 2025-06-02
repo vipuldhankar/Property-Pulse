11a93be91d6bdc3b1abc0cfd78c0ee739f5beab6
vipuldhankar <vipulkumar1823@gmail.com>
2025-06-02 17:13:35 +0530

    message display component done

--- property-pulse/app/api/messages/route.js
+++ property-pulse/app/api/messages/route.js
@@ -5,41 +5,41 @@ import { getSessionUser } from '@/utils/getSessionUser';
 export const dynamic = 'force-dynamic';
 
 // GET /api/messages
-// export const GET = async () => {
-//   try {
-//     await connectDB();
+export const GET = async () => {
+  try {
+    await connectDB();
 
-//     const sessionUser = await getSessionUser();
+    const sessionUser = await getSessionUser();
 
-//     if (!sessionUser || !sessionUser.user) {
-//       return new Response(JSON.stringify('User ID is required'), {
-//         status: 401,
-//       });
-//     }
+    if (!sessionUser || !sessionUser.user) {
+      return new Response(JSON.stringify('User ID is required'), {
+        status: 401,
+      });
+    }
 
-//     const { userId } = sessionUser;
+    const { userId } = sessionUser;
 
-//     const readMessages = await Message.find({ recipient: userId, read: true })
-//       .sort({ createdAt: -1 }) // Sort read messages in asc order
-//       .populate('sender', 'username')
-//       .populate('property', 'name');
+    const readMessages = await Message.find({ recipient: userId, read: true })
+      .sort({ createdAt: -1 }) // Sort read messages in asc order
+      .populate('sender', 'username')
+      .populate('property', 'name');
 
-//     const unreadMessages = await Message.find({
-//       recipient: userId,
-//       read: false,
-//     })
-//       .sort({ createdAt: -1 }) // Sort read messages in asc order
-//       .populate('sender', 'username')
-//       .populate('property', 'name');
+    const unreadMessages = await Message.find({
+      recipient: userId,
+      read: false,
+    })
+      .sort({ createdAt: -1 }) // Sort read messages in asc order
+      .populate('sender', 'username')
+      .populate('property', 'name');
 
-//     const messages = [...unreadMessages, ...readMessages];
+    const messages = [...unreadMessages, ...readMessages];
 
-//     return new Response(JSON.stringify(messages), { status: 200 });
-//   } catch (error) {
-//     console.log(error);
-//     return new Response('Something went wrong', { status: 500 });
-//   }
-// };
+    return new Response(JSON.stringify(messages), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
 
 // POST /api/messages
 export const POST = async (request) => {
---
+++ property-pulse/app/messages/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Messages from '@/components/Messages'
+
+const MessagePage = () => {
+  return (
+    <Messages />
+  )
+}
+
+export default MessagePage
