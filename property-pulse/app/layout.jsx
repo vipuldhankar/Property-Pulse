ad0e8b6ba0930eb91bde84e1e6ab280efc0b1717
vipuldhankar <vipulkumar1823@gmail.com>
2025-06-02 19:40:20 +0530

    message count display on navbar and if we click button mark as read then state will be updated also i used global context to share unread message count between components (Message.jsx ,UnreadMessageCount.jsx)

--- property-pulse/app/layout.jsx
+++ property-pulse/app/layout.jsx
@@ -2,8 +2,9 @@ import Navbar from "@/components/Navbar";
 import "@/assets/styles/globals.css";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { GlobalProvider } from "@/context/GlobalContext";
 
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
@@ -12,16 +13,18 @@ export const metadata = {
 };
 const Mainlayout = ({ children }) => {
   return (
-    <AuthProvider  >
-      <html lang="en">
-        <body>
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
-          <ToastContainer />  {/* For notification */}
-        </body>
-      </html>
-    </AuthProvider>
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang="en">
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer /> {/* For notification */}
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   );
 };
 
