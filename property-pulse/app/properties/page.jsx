f74ad5d90388e9a83b3185913905f77100727b89
vipuldhankar <vipulkumar1823@gmail.com>
2025-06-03 11:20:18 +0530

    pagination fuctionality done

--- property-pulse/app/properties/page.jsx
+++ property-pulse/app/properties/page.jsx
@@ -1,15 +1,9 @@
-import PropertyCard from "@/components/PropertyCard";
-import { fetchproperties } from "@/utils/requests";
 import { getSessionUser } from "@/utils/getSessionUser";
 import cloudinary from "@/config/cloudinary";
 import PropertySearchForm from "@/components/PropertySearchForm";
+import Properties from "@/components/Properties";
 
 const PropertiesPage = async () => {
-  const response = await fetchproperties();
-  const properties = response.properties || []; // Access the properties array
-
-  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
   return (
     <>
       <section className="bg-blue-700 py-4">
@@ -17,20 +11,7 @@ const PropertiesPage = async () => {
           <PropertySearchForm />
         </div>
       </section>
-
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties.length === 0 ? (
-            <p>No Properties Found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 };
